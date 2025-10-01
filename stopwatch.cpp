d71d7232122928854a5b6cefea04cdf30e564c04
Sxllaire <space.shundere@gmail.com>
2025-10-01 14:41:21 -0500

    Create stopwatch.cpp

---
+++ stopwatch.cpp
@@ -0,0 +1,68 @@
+// *************************************************************************** 
+// 
+//   Your Name 
+//   Your Harper ID
+//   Your Course and Section Number 
+// 
+//   I certify that this is my own work and where appropriate an extension 
+//   of the starter code provided for the assignment. 
+// ***************************************************************************
+
+#include "stopwatch.h"
+#include <sstream>
+
+// Constructor timer starts
+StopWatch::StopWatch() : running(false), totalElapsed(0) {}
+
+// Constructor optionally start now
+StopWatch::StopWatch(bool startNow) : running(false), totalElapsed(0) {
+    if (startNow) {
+        start();
+    }
+}
+
+void StopWatch::start() {
+    startTime = std::chrono::steady_clock::now();
+    running = true;
+    totalElapsed = 0; // reset elapsed when starting
+}
+
+void StopWatch::stop() {
+    if (running) {
+        endTime = std::chrono::steady_clock::now();
+        totalElapsed = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime).count();
+        running = false;
+    }
+}
+long StopWatch::elapsed() const {
+    if (running) {
+        auto now = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::seconds>(now - startTime).count();
+    } else {
+        return totalElapsed;
+    }
+}
+
+std::string StopWatch::toString() const {
+    std::ostringstream out;
+    out << elapsed() << " seconds";
+    return out.str();
+}
+
+// Add two watches
+StopWatch StopWatch::add(const StopWatch& sw1, const StopWatch& sw2) {
+    StopWatch result;
+    result.totalElapsed = sw1.elapsed() + sw2.elapsed();
+    result.running = false;
+    return result;
+}
+
+// Subtract two watches
+StopWatch StopWatch::subtract(const StopWatch& sw1, const StopWatch& sw2) {
+    StopWatch result;
+    long diff = sw1.elapsed() - sw2.elapsed();
+    if (diff < 0) diff = 0;
+    result.totalElapsed = diff;
+    result.running = false;
+    return result;
+}
